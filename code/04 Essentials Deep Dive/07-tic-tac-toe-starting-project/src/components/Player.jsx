4034b099b4f7165bf36d1b6282e36d01141ea456
Hongbo <lu.hongbo33@gmail.com>
2024-03-20 18:03:39 -0400

    [UDEMY-MAXIM][Hongbo] feat: Add edit button

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,12 +1,31 @@
+import {useState} from "react";
+
 function Player({name, symbol}) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEdit() {
+    setIsEditing(true);
+  }
+
+  function handleSave() {
+    setIsEditing(false);
+  }
+
+  if (isEditing) {
+    return (
+      <span className="player">
+        <input type="text"/>
+        <span className="player-symbol">{symbol}</span>
+        <button onClick={handleSave}>Save</button>
+      </span>
+    )
+  }
+
   return (
     <span className="player">
-      <span className="player-name">
-        {name}
-      </span>
-      <span className="player-symbol">
-        {symbol}
-      </span>
+      <span className="player-name">{name}</span>
+      <span className="player-symbol">{symbol}</span>
+      <button onClick={handleEdit}>Edit</button>
     </span>
   );
 }
