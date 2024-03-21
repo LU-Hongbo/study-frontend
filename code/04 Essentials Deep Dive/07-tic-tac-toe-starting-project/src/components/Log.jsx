05c76c00cfd574afb3a1d4394ccce7ee049c3268
Hongbo <lu.hongbo33@gmail.com>
2024-03-20 20:55:48 -0400

    [UDEMY-MAXIM][Hongbo] fix: Add Log key

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Log.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Log.jsx
@@ -2,7 +2,7 @@ function Log({gameTurns}) {
   return (
     <ol id="log">
       {gameTurns.map(turn =>
-        <li>
+        <li key={`${turn.rowIndex} ${turn.colIndex}`}>
           {turn.symbol} clicked ({turn.rowIndex},{turn.colIndex})
         </li>
       )}
