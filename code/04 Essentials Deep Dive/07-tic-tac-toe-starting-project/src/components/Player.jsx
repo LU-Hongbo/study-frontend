f9970ac74bf6d961c59b254e1929ed5319f02e37
Hongbo <lu.hongbo33@gmail.com>
2024-03-21 11:38:05 -0400

    [UDEMY-MAXIM][Hongbo] feat: Implement edit and save player name

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,14 +1,44 @@
 import {PLAYER} from "../constant.js";
+import {useState} from "react";
 
 function Player({symbol}) {
-  const name = PLAYER[symbol];
+  const initialName = PLAYER[symbol];
+  const [name, setName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
 
+  function handleOnChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleEdit() {
+    setIsEditing(true);
+  }
+
+  function handleSave() {
+    setIsEditing(false);
+  }
+
+  if (isEditing) {
+    return (
+      <li>
+        <span className="player">
+          <input type="text" value={name} required onChange={handleOnChange}/>
+          <span className="player-symbol">{symbol}</span>
+          <button onClick={handleSave}>
+            Save
+          </button>
+        </span>
+      </li>
+    );
+  }
   return (
     <li>
       <span className="player">
         <span className="player-name">{name}</span>
         <span className="player-symbol">{symbol}</span>
-        <button>Edit</button>
+        <button onClick={handleEdit}>
+          Edit
+        </button>
       </span>
     </li>
   );
