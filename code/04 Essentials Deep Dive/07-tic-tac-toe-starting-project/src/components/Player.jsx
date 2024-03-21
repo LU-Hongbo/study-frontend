98a6712f70ab3870b47d594e830defd2f40d647f
Hongbo <lu.hongbo33@gmail.com>
2024-03-21 14:37:58 -0400

    [UDEMY-MAXIM][Hongbo] feat: Implement App and Player

---
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,14 @@
+function Player() {
+  return (
+    <span className="player">
+      <span className="player-name">
+        TEST NAME
+      </span>
+      <span className="player-symbol">
+        T
+      </span>
+    </span>
+  );
+}
+
+export default Player;
