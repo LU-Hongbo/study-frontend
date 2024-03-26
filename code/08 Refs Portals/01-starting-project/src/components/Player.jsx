27951c108dbcbbb9a59f6f4a7d7977725f7dcf34
Hongbo <lu.hongbo33@gmail.com>
2024-03-26 10:59:56 -0400

    [UDEMY-MAXIM][Hongbo] feat: Implemented TimerChallenge

--- code/08 Refs Portals/01-starting-project/src/components/Player.jsx
+++ code/08 Refs Portals/01-starting-project/src/components/Player.jsx
@@ -1,10 +1,19 @@
+import {useState, useRef} from "react";
+
 export default function Player() {
+  const refName = useRef();
+  const [name, setName] = useState(null);
+
+  function handleClick() {
+    setName(refName.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {name ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={refName} type="text"/>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
