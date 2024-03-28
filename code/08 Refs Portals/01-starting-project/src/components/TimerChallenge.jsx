018e6a38883275b445f21bf6c451d3fa290a0776
Hongbo <lu.hongbo33@gmail.com>
2024-03-27 22:01:51 -0400

    [UDEMY-MAXIM][Hongbo] feat: Implement score system

--- code/08 Refs Portals/01-starting-project/src/components/TimerChallenge.jsx
+++ code/08 Refs Portals/01-starting-project/src/components/TimerChallenge.jsx
@@ -1,15 +1,48 @@
+import {useRef, useState} from "react";
+import ResultModal from "./ResultModal.jsx";
+
 function TimerChallenge({mode, time}) {
+  const timer = useRef();
+  const dialog = useRef();
+
+  const timeInMS = time * 1000;
+  const [remainingTime, setRemainingTime] = useState(timeInMS);
+  const isActive = remainingTime > 0 && remainingTime < timeInMS;
+
+  if (remainingTime <= 0) {
+    onStop();
+  }
+
+  function onStart() {
+    timer.current = setInterval(() => {
+      setRemainingTime(prevTime => prevTime - 10);
+    }, 10);
+  }
+
+  function onStop() {
+    clearInterval(timer.current);
+    dialog.current.open();
+  }
+
+  function onReset() {
+    setRemainingTime(timeInMS);
+  }
+
   return (
-    <section className="challenge">
-      <h2>{mode}</h2>
-      <p className="challenge-time">
-        {time} second{time > 1 ? "s" : ""}
-      </p>
-      <button>
-        Start Challenge
-      </button>
-      <p>Timer inactive</p>
-    </section>
+    <>
+      <ResultModal ref={dialog} time={time} remainingTime={remainingTime} onReset={onReset}/>
+      <section className="challenge">
+        <h2>{mode}</h2>
+        <p className="challenge-time">
+          {time} second{time > 1 ? "s" : ""}
+        </p>
+        {!isActive && <button onClick={onStart}>Start Challenge</button>}
+        {!isActive && <p>Timer inactive</p>}
+
+        {isActive && <button onClick={onStop}>Stop Challenge</button>}
+        {isActive && <p className="active">Timer active</p>}
+      </section>
+    </>
   );
 }
 
